f7a027d666649b91b8ffd73621ad3bfb3dbd31c2
LitvinenkoEvgeny <evgenylitvinenkofrontend@gmail.com>
2015-10-22 00:50:11 +0300

    OOP style

--- upworkjson.py
+++ upworkjson.py
@@ -1,17 +1,25 @@
 import json
 import urllib2
 
-data = urllib2.urlopen(
-    "https://raw.githubusercontent.com/BurntSushi/nflgame/master/nflgame/players.json"
-)
-resp = json.loads(data.read())
-print(dir(resp))
 
-for x in resp.items():
-    full_name = x[1][u"full_name"]
-    try:
-        position = x[1][u"position"]
-        team = x[1][u"team"]
-        print("{} - {} ({})".format(full_name, position, team))
-    except KeyError:
-        continue
+class Players(object):
+    def __init__(self):
+        self.data = urllib2.urlopen(
+            "https://raw.githubusercontent.com/BurntSushi/nflgame/master/nflgame/players.json"
+        )
+        self.resp = json.loads(self.data.read())
+
+    def print_result(self):
+        for x in self.resp.items():
+            full_name = x[1][u"full_name"]
+            try:
+                position = x[1][u"position"]
+                team = x[1][u"team"]
+                print("{} - {} ({})".format(full_name, position, team))
+            except KeyError:
+                continue
+
+
+if __name__ == "__main__":
+    players = Players()
+    players.print_result()
